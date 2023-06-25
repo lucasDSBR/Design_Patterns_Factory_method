d4ac55255c0744a4d3592c79746b3694c1106b58
Lucas da Silva Maciel <lucas.silva@olostech.com>
2023-06-25 11:00:49 -0300

    D-25062023 implementando Factory Method

---
+++ Factory_method/Factory_method/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+namespace RefactoringGuru.DesignPatterns.FactoryMethod.Conceptual
+{
+    abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+            var result = "Criador: O mesmo código de criadores acabou de trabalhar com " + product.Operation();
+
+            return result;
+        }
+
+    }
+
+    class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+
+    class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
+
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Resultado do ConcreteProduct1}";
+        }
+    }
+
+
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Resultado do ConcreteProduct2}";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Lançado com o ConcreteCreator1.");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Lançado com o ConcreteCreator2.");
+            ClientCode(new ConcreteCreator2());
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Cliente: Não tenho conhecimento da classe do criador, " +
+                "mas ainda funciona.\n" + creator.SomeOperation());
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                new Client().Main();
+            }
+        }
+    }
+}
